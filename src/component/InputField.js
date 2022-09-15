e562da626ff0a78a698d22d4de861a385d73d745
arminsultana <arminsultana556@gmail.com>
2022-09-15 15:03:23 +0600

    searc functionality done

--- src/component/InputField.js
+++ src/component/InputField.js
@@ -2,19 +2,23 @@ import React, { useEffect, useState } from 'react';
 
 const InputField = () => {
     const [product, setProduct] = useState([])
+    const [query, setQuery]= useState("")
+   
     useEffect(() => {
         fetch("products.json")
             .then(res => res.json())
             .then(data => setProduct(data))
     })
+   
     return (
-        <div className="flex mid-container gap-2">
-        <div class="form-control ">
+        <div className="flex justify-between mid-container ">
+       <div className="flex gap-2">
+       <div class="form-control gap-2">
              <div class="input-group">
                  <select class="select select-bordered">
                      <option disabled selected>- Hoodies</option>
                      {
-                        product.map(p=><option>{p.name}</option>)
+                        product.map(p=><option key ={p.id}>{p.name}</option>)
                      }
                      
                     
@@ -27,13 +31,22 @@ const InputField = () => {
                  <select class="select select-bordered">
                      <option className='text-bold' disabled selected>Size</option>
                      {
-                        product.map(p=><option>{p.size}</option>)
+                        product.filter(p=>p.name.toLowerCase().includes(query)).map(p=><option key ={p.id}>{p.size}</option>)
                      }
                     
                      
                  </select>
                
              </div>
+         </div>
+       
+       </div>
+       <div className="mr-52 flex gap-2">
+        <div className="">
+            <span className="mr-2">Search:</span>
+             <input type="text" placeholder="Search" class="input input-bordered " onChange={(e)=>setQuery(e.target.value)} /></div>
+        
+         <button className='bg-info text-white px-5 py-2'>Add To Cart</button>
          </div>
         </div>
     );
--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -4,15 +4,54 @@ import { BsCart3 } from 'react-icons/bs';
 
 const Product = () => {
     const [product, setProduct] = useState([])
+    const [query, setQuery] = useState("")
     useEffect(() => {
         fetch("products.json")
             .then(res => res.json())
             .then(data => setProduct(data))
     })
     return (
-        <div className="mt-20">
-            <InputField></InputField>
-          
+        <div className="mt-20 mb-20">
+
+            <div className="flex justify-between mid-container ">
+                <div className="flex gap-2">
+                    <div class="form-control gap-2">
+                        <div class="input-group">
+                            <select class="select select-bordered">
+                                <option disabled selected>- Hoodies</option>
+                                {
+                                    product.map(p => <option key={p.id}>{p.name}</option>)
+                                }
+
+
+                            </select>
+
+                        </div>
+                    </div>
+                    <div class="form-control ">
+                        <div class="input-group">
+                            <select class="select select-bordered">
+                                <option className='text-bold' disabled selected>Size</option>
+                                {
+                                    product.map(p => <option key={p.id}>{p.size}</option>)
+                                }
+
+
+                            </select>
+
+                        </div>
+                    </div>
+
+                </div>
+                <div className="mr-52 flex gap-2">
+                    <div className="">
+                        <span className="mr-2">Search:</span>
+                        <input type="text" placeholder="Search" class="input input-bordered " onChange={(e) => setQuery(e.target.value)} /></div>
+
+                    <button className='bg-info text-white px-5 py-2'>Add To Cart</button>
+                </div>
+            </div>
+
 
 
             <div class="overflow-x-auto w-full mid-container ">
@@ -27,20 +66,20 @@ const Product = () => {
                             <th>Stock</th>
                             <th>Price</th>
                             <div className="w-full">
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                            <th>Buy</th>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                                <th>Buy</th>
                             </div>
-                            
+
 
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            product.map(p => <tr>
+                            product.filter(p=>p.name.toLowerCase().includes(query)).map(p => <tr key={p.id}>
 
                                 <td>
                                     <div class="flex items-center space-x-3">
@@ -57,19 +96,19 @@ const Product = () => {
                                 <td>{p.Stock}</td>
                                 <td>{p.price}</td>
                                 <div className="ml-21">
-                                <td>
-                                <button className='bg-base-200 px-5 py-1 '>1</button></td>
-                                <td><button class="btn btn-sm "><BsCart3></BsCart3></button></td>
-                                <td>   <input type="checkbox" class="checkbox"/></td>
+                                    <td>
+                                        <button className='bg-base-200 px-5 py-1 '>1</button></td>
+                                    <td><button class="btn btn-sm "><BsCart3></BsCart3></button></td>
+                                    <td>   <input type="checkbox" class="checkbox" /></td>
                                 </div>
 
                             </tr>)
-                            
-                            
-                            
+
+
+
                         }
 
-                     
+
 
                     </tbody>
                     {/* <!-- foot --> */}
